055fff47070bcd61a3da7444b6a86b9d9e3ee5f3
shimon <shimon066536@gmail.com>
2025-05-26 17:36:57 +0300

    Create zoo_simulation.py

---
+++ oop/zoo_simulation.py
@@ -0,0 +1,103 @@
+class Animal:
+    _zoo_name = "Hayaton"
+
+    def __init__(self, name, hunger=0):
+        self._name = name
+        self._hunger = hunger
+
+    def get_name(self):
+        print("Hi, my name is", self._name)
+
+    def is_hungry(self):
+        return self._hunger > 0
+
+    def feed(self):
+        self._hunger -= 1
+        self.talk()
+
+    def talk(self):
+        pass
+
+
+class Dog(Animal):
+    def talk(self):
+        print("woof woof")
+
+    def fetch_stick(self):
+        print("There you go, sir!")
+
+
+class Cat(Animal):
+    def talk(self):
+        print("meow")
+
+    def chase_laser(self):
+        print("Meeeeow")
+
+
+class Skunk(Animal):
+    def __init__(self, name, hunger=0, stink_count=6):
+        super().__init__(name, hunger)
+        self._stink_count = stink_count
+
+    def talk(self):
+        print("tsssss")
+
+    def stink(self):
+        print("Dear lord!")
+
+
+class Unicorn(Animal):
+    def talk(self):
+        print("Good day, darling")
+
+    def sing(self):
+        print("I'm not your toy...")
+
+
+class Dragon(Animal):
+    def __init__(self, name, hunger=0, color="Green"):
+        super().__init__(name, hunger)
+        self._color = color
+
+    def talk(self):
+        print("Raaaawr")
+
+    def breath_fire(self):
+        print("$@#$#@$")
+
+
+def main():
+    animals = [
+        Dog("Brownie", 10),
+        Cat("Zelda", 3),
+        Skunk("Stinky", 0),
+        Unicorn("Keith", 7),
+        Dragon("Lizzy", 1450),
+        Dog("Doggo", 80),
+        Cat("Kitty", 80),
+        Skunk("Stinky Jr.", 80),
+        Unicorn("Clair", 80),
+        Dragon("McFly", 80),
+    ]
+
+    for animal in animals:
+        print("The type of the class is", animal.__class__.__name__)
+        animal.get_name()
+        while animal.is_hungry():
+            animal.feed()
+        animal.talk()
+
+        if isinstance(animal, Dog):
+            animal.fetch_stick()
+        elif isinstance(animal, Cat):
+            animal.chase_laser()
+        elif isinstance(animal, Skunk):
+            animal.stink()
+        elif isinstance(animal, Unicorn):
+            animal.sing()
+        elif isinstance(animal, Dragon):
+            animal.breath_fire()
+
+if __name__ == "__main__":
+    main()
