6451fd41c484a79b5bd1eb8137c0bfbb0b0298b0
shimon <shimon066536@gmail.com>
2025-06-03 16:39:57 +0300

    Update fibonacci_generator.py

--- generators_exercises/fibonacci_generator.py
+++ generators_exercises/fibonacci_generator.py
@@ -1,3 +1,9 @@
+"""
+fibonacci_generator.py
+
+This script demonstrates a generator that yields Fibonacci numbers under 100.
+"""
+
 def get_fibo():
     number1 = 0
     number2 = 1
@@ -7,8 +13,10 @@ def get_fibo():
         number1 += number2
         number2 += number1
 
+def main():
+    fibo_gen = get_fibo()
+    for _ in range(6):
+        print(next(fibo_gen))
 
-fibo_gen = get_fibo()
-print(next(fibo_gen))
-print(next(fibo_gen))
-print(next(fibo_gen))
+if __name__ == "__main__":
+    main()
