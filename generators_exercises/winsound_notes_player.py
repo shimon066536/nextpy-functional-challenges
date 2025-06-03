53da18feb6f615a865a9530b6fddb5a6b9d8fd95
shimon <shimon066536@gmail.com>
2025-06-03 17:18:33 +0300

    restructure and document
    - Wrapped logic in main() functions
    - Added module-level docstrings
    - Ensured scripts are executable directly
    - Improved readability and modularity

--- generators_exercises/winsound_notes_player.py
+++ generators_exercises/winsound_notes_player.py
@@ -1,19 +1,34 @@
+"""
+winsound_notes_player.py
+
+Plays a melody using the winsound module and a frequency mapping.
+"""
+
 import winsound
 
-freqs = {"la": 220,
-         "si": 247,
-         "do": 261,
-         "re": 293,
-         "mi": 329,
-         "fa": 349,
-         "sol": 392,
-         }
+freqs = {
+    "la": 220,
+    "si": 247,
+    "do": 261,
+    "re": 293,
+    "mi": 329,
+    "fa": 349,
+    "sol": 392,
+}
 
+def parse_notes(note_string):
+    note_list = note_string.split("-")
+    for note in note_list:
+        tone, dur = note.split(",")
+        yield freqs[tone], int(dur)
 
-notes = "sol,250-mi,250-mi,500-fa,250-re,250-re,500-do,250-re,250-mi,250-fa,250-sol,250-sol,250-sol,500"
+def play_song(song):
+    for freq, dur in parse_notes(song):
+        winsound.Beep(freq, dur)
 
-new_notes = notes.split("-")
+def main():
+    notes = "sol,250-mi,250-mi,500-fa,250-re,250-re,500-do,250-re,250-mi,250-fa,250-sol,250-sol,250-sol,500"
+    play_song(notes)
 
-yonatan_hakatan = (winsound.Beep([freqs[i] for i in [i[0] for i in [note.split(',') for note in new_notes]]][i], [int(i[1]) for i in [note.split(',') for note in new_notes]][i]) for i in range(len(new_notes)))
-
-[i for i in yonatan_hakatan]
+if __name__ == "__main__":
+    main()
