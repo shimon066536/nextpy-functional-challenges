1a903ce8557d24677f0556e4df71cce69cedb4c2
shimon <shimon066536@gmail.com>
2025-06-03 16:28:25 +0300

    Update wallet_combinations.py

--- generators_exercises/wallet_combinations.py
+++ generators_exercises/wallet_combinations.py
@@ -1,9 +1,13 @@
 import itertools
 
+def main():
+    for i in range(len(wallet)):
+        iter = (itertools.combinations(wallet, i))
+        for i in iter:
+            if sum(i) == 100:
+                print(i)
+
 wallet = [20, 20, 20, 10, 10, 10, 10, 10, 5, 5, 1, 1, 1, 1, 1]
 
-for i in range(len(wallet)):
-    iter = (itertools.combinations(wallet, i))
-    for i in iter:
-        if sum(i) == 100:
-            print(i)
+if __name__ == "__main__":
+    main()
