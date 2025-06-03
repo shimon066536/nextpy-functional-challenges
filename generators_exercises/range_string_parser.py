75396adf46bccd27b36858c353b79f874b14bc72
shimon <shimon066536@gmail.com>
2025-06-03 16:47:16 +0300

    restructure and document
    - Wrapped logic in main() functions
    - Added module-level docstrings
    - Ensured scripts are executable directly
    - Improved readability and modularity

--- generators_exercises/range_string_parser.py
+++ generators_exercises/range_string_parser.py
@@ -1,12 +1,20 @@
-def parse_ranges(ranges_string):
-    num_range  = (i for i in ranges_string.split(',')) # list split by ','         for example ['1-2', '4-4', '8-10']
-    ranges     = (num.split('-') for num in num_range) # list in list split by '-' for example [['1', '2'], ['4', '4'], ['8', '10']]
-    result     = (range(int(x[0]), int(x[1])+1) for x in ranges) # convert list to range range(1, 3) range(4, 5) range(8, 11)
-    new_result = (x for y in result for x in y) # print all the ranges to list
+"""
+range_string_parser.py
 
+Parses a string like '1-2,4-4,8-10' into a list of integers using generator expressions.
+"""
+
+def parse_ranges(ranges_string):
+    num_range  = (i for i in ranges_string.split(','))
+    ranges     = (num.split('-') for num in num_range)
+    result     = (range(int(x[0]), int(x[1]) + 1) for x in ranges)
+    new_result = (x for y in result for x in y)
     return new_result
 
+def main():
+    example = "1-2,4-4,8-10"
+    print(f"Parsing ranges from: {example}")
+    print(list(parse_ranges(example)))
 
-print(list(parse_ranges("1-2,4-4,8-10")))
-print(list(parse_ranges("0-0,4-8,20-21,43-45")))
-print(list(parse_ranges("0-0,4-8,20-21,43-45,103-125")))
+if __name__ == "__main__":
+    main()
